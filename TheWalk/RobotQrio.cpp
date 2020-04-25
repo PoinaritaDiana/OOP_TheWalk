0e3777fd871b05b14887dc2e906516518d588e59
PoinaritaDiana <61639111+PoinaritaDiana@users.noreply.github.com>
2020-04-26 01:50:16 +0300

    Add RobotQrio:chooseNewPosition

--- TheWalk/RobotQrio.cpp
+++ TheWalk/RobotQrio.cpp
@@ -6,42 +6,76 @@
 pair<int, int> RobotQrio::chooseNewPosition(const Harta& h) const {
 	/*
 		Robotul Qrio este foarte inteligent, dar nu poate vedea decat o pozitie in jurul sau (are vizibilitate 1).
-		El se poate deplasa in orice directie si incearca pe cat posibil sa evite capcanele, 
-		alegand mereu pozitia disponibila de pe harta care este cat mai aproape de destinatie.
+		El se poate deplasa in orice directie (dar nu poate trece printr-un loc prin care a mai trecut)
+		si incearca pe cat posibil sa evite capcanele, alegand mereu pozitia disponibila de pe harta care este cat mai aproape de destinatie.
 		Daca nu are de ales si este inconjurat de capcane, ori va dezamorsa bomba (daca stie cum sa faca acest lucru),
 		ori isi va sacrifica una din vieti pentru a ajunge la destinatie (alegand, desigur, pozitia care este cat mai aproape de destinatie).
-		Daca intalneste un item corespunzator tipului sau, robotul Qrio va invata cum sa dezamorseze o bomba.
-		Daca intalneste un item care nu ii corespunde, robotul Qrio nu are voie sa il ia.
 	*/
 
+	/*
+		p -> retine coordonatele pozitiei pentru care distanta este minima (daca acolo nu este capcana)
+		pc -> retine coordonatele pozitiei pentru care distanta este minima (daca acolo este capcana)
+		loc -> coordonatele destinatiei (unde trebuie sa ajunga robotul)
+		dmin -> este distanta minima pana la destinatie (pentru pozitiile pe care nu sunt capcane)
+		dminc -> este distanta minima pana la destinatie (pentru pozitiile pe care sunt capcane)
+	*/
 	pair <int, int> p = make_pair(-1, -1);
+	pair <int, int> pc = make_pair(-1, -1);
 	pair <int, int> loc = h.getLocatie();
-	double dmin, d;
-	dmin = -1;									//dmin este distanta minima pana la destinatie
+	double d, dmin, dminc;
+	dmin = -1;								
+	dminc = -1;
 
+	//Pozitia curenta
 	int i = this->getPosition().first;
 	int j = this->getPosition().second;
+
+	//Verific vecinatatea
 	for (int l = i - 1; l <= i + 1; l++)
 		for (int c = j - 1; c <= j + 1; c++) {
-			if (l >= 0 && l < h.getRows() && c >= 0 && c < h.getColumns()) {
-				if ((l != i || c != j) && h.getMatrix(l, c) != 'X' && h.getMatrix(l, c) != '/') {
+			//Daca este pozitie valida 
+			if (l >= 0 && l < h.getRows() && c >= 0 && c < h.getColumns() && (l != i || c != j)) {
+				//Daca nu e nici capcana si nici nu a mai trecut pe acolo
+				if (h.getMatrix(l, c) != 'X' && h.getMatrix(l, c) != '/') {
 					d = sqrt((loc.first - l) * (loc.first - l) + (loc.second - c) * (loc.second - c));
 					if (dmin == -1) {
 						dmin = d;
-						p = { l,c };
+						p = make_pair(l, c);
 					}
 					else
 						if (d < dmin) {
 							dmin = d;
-							p = { l,c };
+							p = make_pair(l, c);
 						}
 				}
+				else 
+					//Daca e capcana
+					if (h.getMatrix(l, c) != 'X') {
+						d = sqrt((loc.first - l) * (loc.first - l) + (loc.second - c) * (loc.second - c));
+						if (dminc == -1) {
+							dminc = d;
+							pc = make_pair(l, c);
+						}
+						else
+							if (d < dminc) {
+								dminc = d;
+								pc = make_pair(l, c);
+							}
+					}
 			}
 		}
-	return p;
+	//Daca am gasit o pozitie pe care nu se afla capcana, atunci o aleg
+	//Daca nu, aleg pozitia pe care se afla capcana dar este cat mai aproape de destinatie
+	if (p != make_pair(-1, -1))
+		return p;
+	else
+		return pc;
 }
 
 void RobotQrio::itemEffect(char i) {
+	//Daca intalneste un item corespunzator tipului sau, robotul Qrio va invata cum sa dezamorseze o bomba.
+	//Daca intalneste un item care nu ii corespunde, robotul Qrio nu are voie sa il ia.
+
 	if (i == 'T') {
 		cout << "\nAi gasit un item pentru Terminator";
 		cout << "\nDin pacate, Qrio nu poate lua acest item pentru ca nu ii apartine :(";
