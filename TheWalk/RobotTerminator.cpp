b609637e056aed377e776de0d5c32bacb9bdfa43
PoinaritaDiana <61639111+PoinaritaDiana@users.noreply.github.com>
2020-04-29 20:30:32 +0300

    Add weapon

--- TheWalk/RobotTerminator.cpp
+++ TheWalk/RobotTerminator.cpp
@@ -1,1 +1,14 @@
 #include "RobotTerminator.h"
+/*
+pair<int, int> RobotTerminator::chooseNewPosition(const Harta& h) const {
+
+}
+*/
+
+void RobotTerminator::moveRobot(Harta&, const int, const int) {
+
+}
+
+void RobotTerminator::itemEffect(char) {
+
+}
--- TheWalk/RobotTerminator.h
+++ TheWalk/RobotTerminator.h
@@ -3,9 +3,10 @@
 
 class RobotTerminator :public Robot {
 	int armor;
+	int weapon;
 
 public:
-	RobotTerminator(const int id) : Robot(id),armor(0) {};
+	RobotTerminator(const int id) : Robot(id), armor(0), weapon(0) {};
 
 	pair<int, int> chooseNewPosition(const Harta& h) const;
 	void moveRobot(Harta&, const int, const int);
