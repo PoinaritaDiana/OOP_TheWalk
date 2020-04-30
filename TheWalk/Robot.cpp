0465dfa32c6bda966155bcb593cf85299f8dbefa
PoinaritaDiana <61639111+PoinaritaDiana@users.noreply.github.com>
2020-04-30 20:46:02 +0300

    Modif Robot

--- TheWalk/Robot.cpp
+++ TheWalk/Robot.cpp
@@ -4,21 +4,12 @@ int Robot::nrVieti = 3;
 
 Robot::Robot(int id): type(id), position({0,0}) {}
 
-void Robot::setPosition(const pair<int,int> p) {
-	this->position = p;
-}
-
-void Robot::setNrVieti() {
-	this->nrVieti--;
-}
-
-void Robot::addViata() {
-	this->nrVieti++;
-}
-
-
 pair<int, int> Robot::getPosition() const {
 	return this->position;
+}
+
+void Robot::setPosition(const pair<int,int> p) {
+	this->position = p;
 }
 
 int Robot::getNrVieti() const {
@@ -28,3 +19,13 @@ int Robot::getNrVieti() const {
 int Robot::getType() const {
 	return this->type;
 }
+
+
+void Robot::decreaseLife() {
+	this->nrVieti--;
+}
+
+void Robot::addLife() {
+	this->nrVieti++;
+}
+
--- TheWalk/Robot.h
+++ TheWalk/Robot.h
@@ -7,24 +7,26 @@ class Harta;
 
 class Robot
 {
-	static int nrVieti;
-	int type;
-	pair <int, int> position;
+	static int nrVieti;				//Numar vieti
+	int type;						//Tipul robotului
+	pair <int, int> position;		//Pozitia robotului
 
 public:
 	Robot(int);
 	virtual ~Robot(){};
 
 	pair<int,int> getPosition() const;				//Getter pentru pozitia curenta a robotului
+	void setPosition(const pair<int, int>);			//Setez noua pozitie a robotului dupa fiecare runda
+
 	int getNrVieti() const;							//Getter pentru numarul de vieti ale robotului
 	int getType() const;							//Getter pentru tipul robotului
-	void setPosition(const pair<int, int>);			//Setez noua pozitie a robotului dupa fiecare runda
-	void setNrVieti();								//Scad numarul de vieti daca este nevoie
-	void addViata();
 
-	virtual pair<int, int> chooseNewPosition(const Harta& h) const = 0;			//Noua pozitie de pe harta a robotului
-	virtual void moveRobot(Harta&, const int, const int) = 0;					//Muta robotul pe noua pozitie aleasa
+	void decreaseLife();							//Scad numarul de vieti
+	void addLife();									//Adaug o viata
+
+	virtual pair<int, int> chooseNewPosition(const Harta& h) const = 0;			//Alege noua pozitie a robotului
+	virtual void moveRobot(Harta&, const int, const int) = 0;					//Muta robotul pe noua pozitie
 	virtual void itemEffect(char) = 0;											//Modeleaza cum se comporta robotul fata de un item
-	virtual void description() = 0;
+	virtual void description() = 0;												//Scurta descriere a robotului
 };
 
